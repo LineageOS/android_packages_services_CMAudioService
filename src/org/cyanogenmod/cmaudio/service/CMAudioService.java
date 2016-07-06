154ec7a0f50d94adb4d20b62eaa8eb8bea1bba99
Roman Birg <roman@cyngn.com>
2016-07-06 13:37:39 -0700

    CMAudioSeervice: initial brokered port from cmsdk
    Change-Id: Ia13e69f121b5258535dbb047c9cb8ef8ea03bc69
    Signed-off-by: Roman Birg <roman@cyngn.com>

---
+++ src/org/cyanogenmod/cmaudio/service/CMAudioService.java
@@ -0,0 +1,169 @@
+package org.cyanogenmod.cmaudio.service;
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.android.internal.content.NativeLibraryHelper;
+
+import android.annotation.SdkConstant;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import cyanogenmod.app.CMContextConstants;
+import cyanogenmod.media.AudioSessionInfo;
+import cyanogenmod.media.CMAudioManager;
+import cyanogenmod.media.ICMAudioService;
+import cyanogenmod.platform.Manifest;
+
+public class CMAudioService extends Service {
+
+    private static final String TAG = "CMAudioService";
+    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private Context mContext;
+    private Handler mHandler = new Handler();
+
+    private static final int AUDIO_STATUS_OK = 0;
+
+    //keep in sync with include/media/AudioPolicy.h
+    private final static int AUDIO_OUTPUT_SESSION_EFFECTS_UPDATE = 10;
+
+    /**
+     * The {@link android.content.Intent} that must be declared as handled by the service.
+     */
+    @SdkConstant(SdkConstant.SdkConstantType.SERVICE_ACTION)
+    public static final String SERVICE_INTERFACE
+            = "cyanogenmod.app.AudioManagerService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+        native_registerAudioSessionCallback(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        native_registerAudioSessionCallback(false);
+        super.onDestroy();
+    }
+
+//    @Override
+//    public void onBootPhase(int phase) {
+//        if (phase == PHASE_BOOT_COMPLETED) {
+//            if (NativeHelper.isNativeLibraryAvailable()) {
+//                native_registerAudioSessionCallback(true);
+//            }
+//        }
+//    }
+
+    private final IBinder mBinder = new ICMAudioService.Stub() {
+
+        @Override
+        public List<AudioSessionInfo> listAudioSessions(int streamType) throws RemoteException {
+            final ArrayList<AudioSessionInfo> sessions = new ArrayList<AudioSessionInfo>();
+//            if (!NativeHelper.isNativeLibraryAvailable()) {
+//                // no sessions for u
+//                return sessions;
+//            }
+
+            int status = native_listAudioSessions(streamType, sessions);
+            if (status != AUDIO_STATUS_OK) {
+                Log.e(TAG, "Error retrieving audio sessions! status=" + status);
+            }
+
+            return sessions;
+        }
+
+        @Override
+        public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
+            mContext.enforceCallingOrSelfPermission(android.Manifest.permission.DUMP, TAG);
+
+            pw.println();
+            pw.println("CMAudio Service State:");
+            try {
+                List<AudioSessionInfo> sessions = listAudioSessions(-1);
+                if (sessions.size() > 0) {
+                    pw.println("  Audio sessions:");
+                    for (AudioSessionInfo info : sessions) {
+                        pw.println("   " + info.toString());
+                    }
+                } else {
+                    pw.println("  No active audio sessions");
+                }
+            } catch (RemoteException e) {
+                // nothing
+            }
+        }
+    };
+
+    private void broadcastSessionChanged(boolean added, AudioSessionInfo sessionInfo) {
+        Intent i = new Intent(CMAudioManager.ACTION_AUDIO_SESSIONS_CHANGED);
+        i.putExtra(CMAudioManager.EXTRA_SESSION_INFO, sessionInfo);
+        i.putExtra(CMAudioManager.EXTRA_SESSION_ADDED, added);
+
+        sendBroadcastToAll(i, Manifest.permission.OBSERVE_AUDIO_SESSIONS);
+    }
+
+    private void sendBroadcastToAll(Intent intent, String receiverPermission) {
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+
+        final long ident = Binder.clearCallingIdentity();
+        try {
+            if (DEBUG) Log.d(TAG, "Sending broadcast: " + intent.toString());
+
+            mContext.sendBroadcastAsUser(intent, UserHandle.ALL, receiverPermission);
+        } finally {
+            Binder.restoreCallingIdentity(ident);
+        }
+    }
+
+    /*
+     * Handles events from JNI
+     */
+    private synchronized void audioSessionCallbackFromNative(int event,
+            AudioSessionInfo sessionInfo, boolean added) {
+
+        switch (event) {
+            case AUDIO_OUTPUT_SESSION_EFFECTS_UPDATE:
+                broadcastSessionChanged(added, sessionInfo);
+                break;
+            default:
+                Log.e(TAG, "Unknown event " + event);
+        }
+    }
+
+    private static native void native_registerAudioSessionCallback(boolean enabled);
+
+    private static native int native_listAudioSessions(
+            int stream, ArrayList<AudioSessionInfo> sessions);
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
